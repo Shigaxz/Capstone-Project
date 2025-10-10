f65f72562f3fac71689c5a0e646c3e3812757827
pieroreip <pie.valdes@duocuc.cl>
2025-10-10 02:58:24 -0300

    cambios
    cambios minimos en los ficheros

--- fase2/evidenciaProyecto/frontend-project/src/vistas/Reservas/SeleccionHorario.tsx
+++ fase2/evidenciaProyecto/frontend-project/src/vistas/Reservas/SeleccionHorario.tsx
@@ -1,26 +1,18 @@
 import './SeleccionHorario.css'
 import { useState, useCallback } from 'react'
-import { useNavigate, useLocation } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
 import DateDisplay from '../../componentes/DateDisplay';
 import HorarioEspacio from '../../componentes/HorarioEspacio';
-
-interface Prueba{
-  nombre:string
-}
 interface DisplayHorarios{
   nombreSede:string,
   espacioSede:string
 }
 function SeleccionHorario(){
 
-const navigate = useNavigate();
 const locate = useLocation()
 const ladata = locate.state as DisplayHorarios
 const [dia , setDia] = useState('');
-const handeaElPrueba = ()=>{
-  const palaprueba:Prueba = {nombre:'pedo'}
-  navigate('/formulario' , {state: palaprueba})
-}
+
 const handleChildData = useCallback((data:string)=>{
   setDia(data);
 },[])
@@ -35,7 +27,6 @@ return(<>
   <div className='bg-amber-300 w-52  py-0.5 rounded-lg shadow text-center font-medium'>{dia}</div>
 </div>
 
-<button onClick={handeaElPrueba}>tocame papi</button>
 
 <HorarioEspacio nombreSede={ladata.nombreSede} espacioSede={ladata.espacioSede}/>
    
